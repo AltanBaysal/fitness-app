d27f99403798f5a0890b7380a308ff3e7ee2880e
AltanBaysal <altanbaysaldeveloper@gmail.com>
2022-07-25 22:27:55 +0300

    Init Added

---
+++ lib/core/init/controller_list.dart
@@ -0,0 +1,10 @@
+
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:provider/single_child_widget.dart';
+
+import '../../features/user_information/presentation/provider/user_information_controller.dart';
+
+List<SingleChildWidget> controllerList = [ 
+  ChangeNotifierProvider<UserInformationController>(create: (BuildContext context) => UserInformationController()),
+];
